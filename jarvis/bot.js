199ef9383f5a40ac8c9fc2263bd8b0f0b58f66c1
Regan Tan <regantanyk@gmail.com>
2025-03-08 18:20:30 +0800

    /echo and /greet custom bot commands

--- jarvis/bot.js
+++ jarvis/bot.js
@@ -6,11 +6,29 @@ const bot = new TelegramBot(token, {
     polling: true
 });
 
-bot.on('message', function(msg) {
-    console.log('Received a message');
+// // Respond to any message of any kind
+// bot.on('message', function(msg) {
+//     console.log('Received a message');
 
-    const res = `Hi ${msg.from.first_name}! I have received your message: ${msg.text}`;
+//     const res = `Hi ${msg.from.first_name}! I have received your message: ${msg.text}`;
+
+//     bot.sendMessage(msg.chat.id, res);
+// });
+
+// Create an ECHO command
+bot.onText(/\/echo (.+)/, function(msg, match) {
+    console.log("Received an echo request");
+    
+    const data = match[1];
+
+    bot.sendMessage(msg.chat.id, data);
+});
+
+// Create a GREET command
+bot.onText(/\/greet (.+)/, function(msg, match) {
+    console.log("Received a greeting");
+
+    const res = `Hello ${match[1]}, how are you doing?`
 
     bot.sendMessage(msg.chat.id, res);
 });
-
