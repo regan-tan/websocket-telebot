39f1631dcabc4e2c8037701520e538003fb0781c
Regan Tan <regantanyk@gmail.com>
2025-03-08 20:30:54 +0800

    minor bug fix

--- jarvis/bot.js
+++ jarvis/bot.js
@@ -33,3 +33,57 @@ bot.onText(/\/greet (.+)/, function(msg, match) {
 
     bot.sendMessage(msg.chat.id, res);
 });
+
+// Create a ball command
+bot.onText(/\/ball (.+)/, function(msg, match) {
+    console.log("Received ball request from Telegram");
+    
+    // console.log(match[1]);
+    const coordinates = match[1].split(' ');
+    const xpos = parseFloat(coordinates[0]);
+    const ypos = parseFloat(coordinates[1]);
+    const coordObj = {
+      x: xpos,
+      y: ypos
+    }
+    
+    // Broadcast that message to all connected clients
+    wsServer.clients.forEach(function (client) {
+      client.send(JSON.stringify(coordObj));
+    });
+});
+
+
+// WEBSOCKET SERVER PART
+
+const WebSocket = require('ws');
+const PORT = 5001;
+const wsServer = new WebSocket.Server({
+    port: PORT
+});
+
+wsServer.on('connection', function (socket) {
+    // Some feedback on the console
+    console.log("A client just connected");
+
+    // Attach some behavior to the incoming socket
+    socket.on('message', function (msg) {
+        console.log("Received message from client: " + msg)
+        // socket.send("Take this back: " + msg)
+
+        try {
+            let parsedMsg = JSON.parse(msg); // Ensure it's an object
+            
+            // Broadcast the parsed message correctly
+            wsServer.clients.forEach(function (client) {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify(parsedMsg)); // Send a proper JSON string
+                }
+            });
+        } catch (error) {
+            console.error("Error parsing received message:", error);
+        }
+    });
+  });
+
+console.log( (new Date()) + " Server is listening on port " + PORT);
