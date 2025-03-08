aec7bff36d74d4f248ffe730761d9b44a2f53c50
Regan Tan <regantanyk@gmail.com>
2025-03-08 11:12:59 +0800

    glitch-specific connection settings implemented

--- client-simple/client.js
+++ client-simple/client.js
@@ -1,8 +1,14 @@
 const WebSocket = require('ws');
 
-const serverAddress= "ws://127.0.0.1:5000";
+// const serverAddress= "ws://127.0.0.1:5000";
 
-const ws = new WebSocket(serverAddress);
+const serverAddress= "wss://transparent-sumptuous-oval.glitch.me";
+
+const ws = new WebSocket(serverAddress, {
+    headers: {
+        "user-agent": "Mozilla"
+    }
+});
 
 ws.on('open', function() {
     ws.send("Hello server!");
